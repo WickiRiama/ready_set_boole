12550f62fca0514443b098b8467ac91395807ecd
Margaux RIANT <mriant@student.42.fr>
2024-01-10 17:13:24 +0100

    Save.
     Changes to be committed:
    	modified:   srcs/adder.cpp
    	modified:   srcs/main.cpp

--- ex00/srcs/adder.cpp
+++ ex00/srcs/adder.cpp
@@ -2,5 +2,5 @@
 
 uint32_t adder(uint32_t a, uint32_t b)
 {
-    
+    return a^b;
 }
--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+
 #include "adder.h"
+
+int main(void)
+{
+    uint32_t a = 2;
+    uint32_t b = 3;
+    std::cout << adder(a, b) << std::endl;
+}
