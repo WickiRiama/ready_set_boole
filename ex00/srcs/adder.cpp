f141a6a5c7330b4f9a14a39476dce6a7f9da496e
Margaux RIANT <mriant@student.42.fr>
2024-01-12 15:21:47 +0100

    Ex00 Adder ok
     Changes to be committed:
    	modified:   srcs/adder.cpp
    	modified:   srcs/main.cpp

--- ex00/srcs/adder.cpp
+++ ex00/srcs/adder.cpp
@@ -1,6 +1,29 @@
+#include <iostream>
+
 #include "adder.h"
 
 uint32_t adder(uint32_t a, uint32_t b)
 {
-    return a^b;
+	uint32_t result = 0;
+	uint32_t tmp;
+	uint32_t tmp_a;
+	uint32_t tmp_b;
+	uint32_t carry = 0;
+	
+	for (int i = 0; i < 32; i++)
+	{
+		tmp_a = a >> i & 1;
+		tmp_b = b >> i & 1;
+
+		tmp = tmp_a ^ tmp_b ^ carry;
+		result = result ^ (tmp<<i);
+
+		if (carry == 1)
+			carry = tmp_a | tmp_b;
+		else
+			carry = tmp_a & tmp_b;
+	}
+	if (carry == 1)
+		std::cerr << "Warning: overflow" << std::endl;
+	return result;
 }
--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
+#include <ctime>
 
 #include "adder.h"
 
 int main(int ac, char** av)
 {
-    if (ac != 3)
-    {
-        std::cerr << "Usage: ./adder uint1 uint2" << std::endl;
-    }
-    uint32_t a = static_cast<uint32_t>(atoi(av[1]));
-    uint32_t b = static_cast<uint32_t>(atoi(av[1]));
-    std::cout << adder(a, b) << std::endl;
+	if (ac != 3)
+	{
+		std::cerr << "Usage: ./adder uint1 uint2" << std::endl;
+		return 1;
+	}
+	uint32_t a = static_cast<uint32_t>(atoi(av[1]));
+	uint32_t b = static_cast<uint32_t>(atoi(av[2]));
+	
+	std::cout << adder(a, b) << std::endl;
 }
