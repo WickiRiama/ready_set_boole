58a8d36455c072b70d3782a75918f227e70ebc43
Margaux RIANT <mriant@student.42.fr>
2024-01-12 13:51:22 +0100

    Add arguments to main
     Changes to be committed:
    	modified:   srcs/main.cpp

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -2,9 +2,13 @@
 
 #include "adder.h"
 
-int main(void)
+int main(int ac, char** av)
 {
-    uint32_t a = 2;
-    uint32_t b = 3;
+    if (ac != 3)
+    {
+        std::cerr << "Usage: ./adder uint1 uint2" << std::endl;
+    }
+    uint32_t a = static_cast<uint32_t>(atoi(av[1]));
+    uint32_t b = static_cast<uint32_t>(atoi(av[1]));
     std::cout << adder(a, b) << std::endl;
 }
