ddb950c11673a9aa9b9a7c6ab4d5deea75592055
Margaux RIANT <mriant@student.42.fr>
2024-02-07 16:48:08 +0100

    Ex04: Change variables structure from map to string

--- ex04/srcs/print_truth_table.cpp
+++ ex04/srcs/print_truth_table.cpp
@@ -1,31 +1,48 @@
 #include <string>
-#include <map>
 #include <iostream>
+#include <algorithm>
 
 #include "print_truth_table.h"
 #include "Ast.hpp"
 
-std::map<char, bool> setVariables(std::string &formula)
+std::string setVariables(std::string &formula)
 {
-	std::map<char, bool> newMap;
+	std::string variables;
 	for (size_t i = 0; i < formula.size(); i++)
 	{
-		if (isalpha(formula[i]) && isupper(formula[i]))
+		if (isalpha(formula[i]) && isupper(formula[i]) && variables.find(formula[i]) == std::string::npos)
 		{
-			newMap[formula[i]] = 0;
+			variables += formula[i];
 		}
 	}
-	return newMap;
+	sort(variables.begin(), variables.end());
+	return variables;
+}
+
+void printHeader(std::string const &variables)
+{
+	std::cout << "|";
+	for (size_t i = 0; i < variables.size(); i++)
+	{
+		std::cout << " " << variables[i] << " |";
+	}
+	std::cout << " = |" << std::endl;
+	std::cout << "|";
+	for (size_t i = 0; i <= variables.size(); i++)
+	{
+		std::cout << "---|";
+	}
+	std::cout << std::endl;
 }
 
 void print_truth_table(std::string &formula)
 {
-	std::map<char, bool> variables = setVariables(formula);
-
-	for (std::map<char, bool>::iterator it = variables.begin(); it != variables.end(); it++)
+	std::string variables = setVariables(formula);
+	if (variables.size() == 0)
 	{
-		std::cout << "[" << it->first << ": " << it->second << "] ";
+		std::cerr << "The formula is invalid" << std::endl;
+		return;
 	}
-	std::cout << std::endl;
 
+	printHeader(variables);
 }
