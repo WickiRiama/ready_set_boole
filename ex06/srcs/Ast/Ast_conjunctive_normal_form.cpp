6407072bc4f07e5a7eba84994ba1d3e303395425
Margaux RIANT <mriant@student.42.fr>
2024-03-08 19:01:56 +0100

    Ex06: Add pseudo code

---
+++ ex06/srcs/Ast/Ast_conjunctive_normal_form.cpp
@@ -0,0 +1,46 @@
+#include "Ast.hpp"
+
+/* 
+Convert2CNN(void)
+{
+	Convert2CNN(this->_root_node)
+}
+
+Convert2CNN(Node *root)
+{
+	si non root ou root = leaf
+		return
+	si & et parent et parent = |
+		root = remonte(&)
+		Convert2CNN(root)
+	sinon
+		Convert2CNN(left_child)
+		Convert2CNN(right_child)
+}
+
+createDisjunction(Node *parent, Node *node1, Node* node2)
+{
+	new node = Node(parent, '|')
+	node->left_child = node1
+	node->right_child = node2
+	return node
+
+}
+
+remonte(*Node conjunction) {
+	disjuncton = conjunction->parent
+	si conjunction == disjunction->left_child
+		A = disjunction->right_child
+	sinon
+		A = disjunction->left_child
+	B = conjunction->left_child
+	C = conjunction->right_child
+
+	disjunction = &
+	dijunction->left_child = createDisjunction(copy(A), B)
+	dijunction->right_child = createDisjunction(A, C)
+
+	return disjunction
+}
+
+*/
